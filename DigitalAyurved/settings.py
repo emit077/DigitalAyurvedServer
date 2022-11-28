7cb09f8238d668a28d3cc9e5c48a4e8758bed0de
amit.sahu <amit.sahu@codenicely.in>
2022-11-29 00:32:26 +0530

    postgresql

--- DigitalAyurved/settings.py
+++ DigitalAyurved/settings.py
@@ -95,12 +95,26 @@ WSGI_APPLICATION = 'DigitalAyurved.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if env('IS_PRODUCTION'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'digitalayurveddb',
+            'USER': 'django_postgres',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
